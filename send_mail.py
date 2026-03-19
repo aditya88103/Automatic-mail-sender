472d40c9e3e62cf77574083ecc462937b35dff93
Aditya <aditya8810328@gmail.com>
2026-03-19 16:21:58 +0530

    Add email validation and mailer script

---
+++ send_mail.py
@@ -0,0 +1,61 @@
+import smtplib
+import csv
+import re
+from email.mime.text import MIMEText
+
+sender_email = "aditya8810328@gmail.com"
+app_password = "kvfe iywg prda pbzg"
+
+subject = "Data Analyst Internship Opportunity"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, app_password)
+
+email_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
+
+with open("emails.csv", newline="", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        name = row["Name"]
+        email = row["Email"]
+        company = row["Company"]
+
+        if not email_pattern.match(email):
+            print("Skipped invalid email:", email)
+            continue
+        
+
+        body = f"""Hi {name},
+
+I came across {company}'s Data Analytics Internship opening on LinkedIn and wanted to reach out directly.
+
+I'm Aditya Raj, a B.Tech student at Invertis University (2023-27, CGPA: 7.9) with hands-on experience in Python, SQL, Power BI, and DAX. A few things I've built recently:
+
+- A Power BI dashboard with 10+ visuals for Super Store Sales & Profit analysis (YoY trends, KPIs using DAX)
+- A PostgreSQL-based Bookstore Sales Analytics project with 15+ complex queries (joins, aggregations, revenue trends)
+- A full Retail EDA using Pandas, NumPy, and Scikit-learn with heatmaps and outlier detection
+
+I also completed the Deloitte Data Analytics Job Simulation (Forage) and hold certifications in Python (IBM), Power BI, and SQL.
+
+I'm available for a 3-6 month internship immediately . I'd love to contribute to {company}'s data team.
+
+Would you be open to a quick 10-minute call, or shall I send my resume for review?
+
+Best regards,
+Aditya Raj
++91 8810328594 | aditya8810328@gmail.com
+LinkedIn: linkedin.com/in/aditya-raj-767822271
+GitHub: github.com/aditya88103
+"""
+
+        msg = MIMEText(body, _charset="utf-8")
+        msg["Subject"] = subject
+        msg["From"] = sender_email
+        msg["To"] = email
+
+        server.sendmail(sender_email, email, msg.as_string())
+        print("Sent to:", email)
+
+server.quit()
